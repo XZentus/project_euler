5f08ed9e6077ea576cb5a64b961bb6a2b6723339
Nex <xz.mxnf@gmailcom>
2022-05-25 16:00:37 +0300

    Removed unnessesary type restrictions

--- src/project_euler.jl
+++ src/project_euler.jl
@@ -2,7 +2,7 @@ module project_euler
 
 export pe_1
 
-function pe_1(from::T=1, to::T=999; timeout::UInt64=UInt64(5_000_000_000)) where T <: Integer
+function pe_1(from::T = 1, to::T = 999; timeout::UInt64 = UInt64(5_000_000_000))
     println("Problem 1")
     println("Direct calculation:")
     @time result = pe_1_direct(from, to, timeout=timeout)
@@ -17,7 +17,7 @@ function pe_1(from::T=1, to::T=999; timeout::UInt64=UInt64(5_000_000_000)) where
     println("Result = ", result)
 end
 
-function pe_1_direct(from::T=1, to::T=999; timeout::UInt64=UInt64(5_000_000_000)) where T <: Integer
+function pe_1_direct(from = 1, to = 999; timeout::UInt64 = UInt64(5_000_000_000))
     startTime = time_ns()
     result = 0
     for x in from:to
@@ -32,14 +32,14 @@ function pe_1_direct(from::T=1, to::T=999; timeout::UInt64=UInt64(5_000_000_000)
     result
 end
 
-function pe_1_sum_ranges(from=1, to=999)
+function pe_1_sum_ranges(from = 1, to = 999)
     start3  = (from %  3 == 0) ? from : (from +  3 - from %  3)
     start5  = (from %  5 == 0) ? from : (from +  5 - from %  5)
     start15 = (from % 15 == 0) ? from : (from + 15 - from % 15)
     sum(start3 : 3 : to) + sum(start5 : 5 : to) - sum(start15 : 15 : to)
 end
 
-function pe_1_analytical(from=1, to=999)
+function pe_1_analytical(from = 1, to = 999)
     start3  = (from %  3 == 0) ? from : (from +  3 - from %  3)
     start5  = (from %  5 == 0) ? from : (from +  5 - from %  5)
     start15 = (from % 15 == 0) ? from : (from + 15 - from % 15)
