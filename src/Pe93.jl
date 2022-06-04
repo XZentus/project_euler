2cfadd5964918b9f00e1d182b6b2d20ebcb8fd4f
XZentus <xz.mxnf@gmail.com>
2022-06-04 23:33:30 +0300

    Problem 93: removed dependency, increased speed

--- src/Pe93.jl
+++ src/Pe93.jl
@@ -1,20 +1,24 @@
 module Pe93
 
-using Combinatorics
-
 export pe_93
 
-function pe_93()
+function pe_93(numbers_limit = 9)
     max_seq_len, values = 0, (0, 0, 0, 0)
-    operators = [(+),  (+),  (+),
-                 (*),  (*),  (*),
-                 (-),  (-),  (-),
-                 (//), (//), (//)]
-    for numbers ∈ combinations([1, 2, 3, 4, 5, 6, 7, 8, 9], 4)
+    operators = [(+),  (-),  (*), (//)]
+    for n1 ∈ 1:9,
+        n2 ∈ (n1 + 1):numbers_limit,
+        n3 ∈ (n2 + 1):numbers_limit,
+        n4 ∈ (n3 + 1):numbers_limit
+
         res = Set{Int}()
-        for ops             ∈ combinations(operators, 3),
-            (a, b, c, d)    ∈ permutations(numbers),
-            (op1, op2, op3) ∈ permutations(ops)
+        ns = [n1, n2, n3, n4]
+        for op1 ∈ operators,
+            op2 ∈ operators,
+            op3 ∈ operators,
+            a ∈ ns, b ∈ ns, c ∈ ns, d ∈ ns
+            (a == b || a == c || a == d ||
+             b == c || b == d ||
+             c == d) && continue
 
             value1 = op1(a, op2(b, op3(c, d)))
             value2 = op1(op2(op3(a, b), c), d)
@@ -31,8 +35,8 @@ function pe_93()
         end
         i -= 1
         if max_seq_len < i
-            max_seq_len, values = i, numbers
-            println("$i ⇐ $numbers")
+            max_seq_len, values = i, [n1 n2 n3 n4]
+            println("$i ⇐ $values")
         end
     end
     println("$(max_seq_len) ⇐ $values")
